f6d105330e4dd1e7f2ac7f32ec8ebad5dbe48d18
MJVaghasiya <maulikvaghasiya51@gmail.com>
2023-08-08 16:40:39 +0530

    some css changes

--- src/components/About.js
+++ src/components/About.js
@@ -24,10 +24,12 @@ const About = () => {
         <div>
             <div className='container overflow-hidden'>
                 <div className='row height-350 height mt-5'>
-                    <div className='col-lg-6 pad-60-60 '>
+                    <div className='col-lg-6'>
+                        <div className="width-500">
                         <h1 >Skill Worker providing high quality care visits at home</h1>
-                        <p >Our services support you to maintain your chosen lifestyle. So, we keep you in control and provide you with the home care and support that you want, where and when you want it.</p>
+                        <p >Our services support you to maintain your chosen lifestyle.  So, we keep you in control and provide you with the home care and support that you want, where and when you want it.</p>
                         <a href='#' className='about-us-link' >Find out more about us</a>
+                        </div>
                     </div>
                     <div className='col-lg-6 mt-15'>
                         <img src={img1} className='w-100 h-70' />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ class Navbar extends React.Component {
       <div>
         <nav class="navbar navbar-expand-lg ">
           <div class="container-fluid nav-container">
-            <a class="navbar-brand" href="#">Skill Worker</a>
+            <a class="navbar-brand" href="/">Skill Worker</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
